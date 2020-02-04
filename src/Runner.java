a56fc5b2c13f2617fda0e7f0f4406da0c52e5a0d
lukerock7 <56939157+lukerock7@users.noreply.github.com>
2020-02-04 13:37:24 -0800

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,21 +1,36 @@
-
 public class Runner {
-    public Runner(){
+    public Runner() {
 
     }
     public static void main(String[] args) {
         System.out.println(makes10(10, 5));
-        System.out.println(frontTimes("Albinson",14/15));
+        System.out.println(frontTimes("Albinson", 14 / 15));
         System.out.println(firstTwo("Hello"));
-        int[] a = {2,3,4};
+        int[] a = {
+            2,
+            3,
+            4
+        };
         System.out.println(fix23(a));
-        System.out.println(teenSum(13,19));
-        System.out.println(loneSum(21,22));
+        System.out.println(teenSum(13, 19));
+        System.out.println(loneSum(21, 22));
         System.out.println(countHi("hihihihihihi"));
         System.out.println(sumDigits("Albinsongivesyou14outof15"));
-        int[] b = {5,4,2,1};
+        int[] b = {
+            5,
+            4,
+            2,
+            1
+        };
         System.out.println(only14(b));
-        int[] c = {1,3,5,7,9,2};
+        int[] c = {
+            1,
+            3,
+            5,
+            7,
+            9,
+            2
+        };
         System.out.println(canBalance(c));
 
     }
@@ -44,7 +59,7 @@ public class Runner {
     }
     //Array-1 - fix23
     public static int[] fix23(int[] nums) {
-        if (nums[0] == 2 && nums[1] == 3){
+        if (nums[0] == 2 && nums[1] == 3) {
             nums[1] = 0;
             return nums;
         }
@@ -56,29 +71,29 @@ public class Runner {
     }
     //Logic-1 - teenSum
     public static int teenSum(int a, int b) {
-        int total = a+b;
+        int total = a + b;
         if ((b >= 13 && b <= 19) || (a >= 13 && a <= 19))
             return 19;
         else
             return total;
     }
     //Logic-2 - blackjack
-public int blackjack(int a, int b) {
-    if (a > 21) a = 0;
-    if (b > 21) b = 0;
+    public int blackjack(int a, int b) {
+        if (a > 21) a = 0;
+        if (b > 21) b = 0;
 
-    if (a > b) {
-        return a;
-    } else {
-        return b;
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
     }
-}
     //String-2 - countHi
     public static int countHi(String str) {
         int count = 0;
 
-        for(int i = 0; i < str.length() - 1; i++) {
-            if(str.substring(i, i + 2).equals("hi"))
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("hi"))
                 count++;
         }
 
@@ -88,8 +103,8 @@ public int blackjack(int a, int b) {
     public static int sumDigits(String str) {
         int total = 0;
 
-        for(int i = 0; i < str.length(); i++) {
-            if(Character.isDigit(str.charAt(i)))
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i)))
                 total = total + str.charAt(i) - '0';
         }
 
@@ -97,8 +112,8 @@ public int blackjack(int a, int b) {
     }
     //Array-2 - only14
     public static boolean only14(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 1 && nums[i] != 4)
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 1 && nums[i] != 4)
                 return false;
         }
 
@@ -109,14 +124,14 @@ public int blackjack(int a, int b) {
         int firstside = 0;
         int secondside = 0;
 
-        for(int i = 0; i < nums.length; i++)
+        for (int i = 0; i < nums.length; i++)
             secondside += nums[i];
 
-        for(int i = 0; i <= nums.length - 2; i++) {
+        for (int i = 0; i <= nums.length - 2; i++) {
             firstside += nums[i];
             secondside -= nums[i];
 
-            if(firstside == secondside)
+            if (firstside == secondside)
                 return true;
         }
 
